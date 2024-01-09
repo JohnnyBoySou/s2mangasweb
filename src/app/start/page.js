7874df92b88655bbfa8751a5c2804ec9583e6b95
JohnnyBoySou <joaodesousa101@gmail.com>
2024-01-09 19:52:22 -0300

    login/register

--- src/app/start/page.js
+++ src/app/start/page.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Column, Row, Title, Label, ButtonPrimaryLight, ButtonOff } from '../../themes/global.js'
 import Image from 'next/image';
+import Link from 'next/link'
 
 export default function Start (){
     const currentVersion = '1.2'
     return(
-        <Column className="banner" style={{backgroundColor: "#ED274A", overflow: 'hidden'}}>
+        <Column  style={{backgroundColor: "#ED274A", overflow: 'hidden', padding: 44, borderRadius: 8, marginTop: 12,flexGrow: 1, marginBottom: 12, }}>
         <Image src="/icon.png" alt="logo s2mangas icon" width={64} height={64} className='imgicon'/>
             <Column className='marq'>
                 <Title className="marqueeStyle" style={{fontFamily: 'Book',}}>✷ COLEÇÕES ✷ CARDS ✷ CURTIR ✷ SEGUIR ✷ AVATAR ✷ CAPA ✷ MIX ✷ </Title>
@@ -16,8 +17,12 @@ export default function Start (){
           <Title style={{fontSize: 42,}}>Simples. Intuítivo. Rápido.</Title>
           <Title style={{fontFamily: 'Book',}}>Um leitor de mangás completo. </Title>
           <Row style={{marginTop: 20,}}>
-            <ButtonPrimaryLight>Começar agora</ButtonPrimaryLight>
-            <ButtonOff style={{marginLeft: 15,}}>Entrar</ButtonOff>
+            <Link href={`/register`} >
+              <ButtonPrimaryLight>Começar agora</ButtonPrimaryLight>
+            </Link>
+            <Link href={`/login`} >
+              <ButtonOff style={{marginLeft: 15,}}>Entrar</ButtonOff>
+            </Link>
           </Row>
         </Column>
       )
