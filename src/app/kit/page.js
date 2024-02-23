fdffb4517609323c62cdd536a078415c7b3ced12
JohnnyBoySou <joaodesousa101@gmail.com>
2024-02-22 22:38:47 -0300

    Removido importação desnecessária e código não utilizado

--- src/app/kit/page.js
+++ src/app/kit/page.js
@@ -1,13 +1,10 @@
 'use client'
 import React, {useState} from 'react';
-import Image from 'next/image';
 import {  Column, Row, Title, Label, } from './../../themes/global';
 import Progress from '../../components/Kit/Progress';
 import Check from '../../components/Kit/Check';
-import Input from '../../components/Kit/input';
 
 export default function Kit (){
-
     const [check, setCheck] = useState(false);
     const [value, setValue] = useState('a');
 return(
@@ -17,8 +14,6 @@ return(
     <Column>
         <Progress porcentage={12}/>
         <Check value={check} onClick={() => setCheck(!check)}/>
-        <Input value={value} onClick={(value) => setValue(value)}/>
-        
     </Column>
   
 </Column>
