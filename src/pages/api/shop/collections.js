a2238ce1801c5d85201db6166b17b8efdea44b50
JohnnyBoySou <joaodesousa101@gmail.com>
2024-03-06 06:06:54 -0300

    backgrounds correction

--- src/pages/api/shop/collections.js
+++ src/pages/api/shop/collections.js
@@ -1,8 +1,8 @@
-import { geral } from '../../../requests/shop/collections';
+import { backgrounds } from '../../../requests/shop/collections';
 
 export default async function handler(req, res) {
   try {
-    res.status(200).json(geral);
+    res.status(200).json(backgrounds);
   } catch (error) {
     console.error(error.message);
     res.status(500).json({ error: 'Erro na requisição' });
