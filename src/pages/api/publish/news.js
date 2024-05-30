dc6d18936cbe6ba423cbf1ba11f284226095de2c
JohnnyBoySou <joaodesousa101@gmail.com>
2024-05-30 17:48:53 -0300

    news endpoints

---
+++ src/pages/api/publish/news.js
@@ -0,0 +1,14 @@
+import news from "../../../data/news";
+export default async function handler(req, res) {
+    try {
+      res.status(200).json({ mangas: news });
+    } catch (error) {
+      console.error('Axios error:', error.message);
+      console.error('Status:', error.response ? error.response.status : 'unknown');
+      console.error('Data:', error.response ? error.response.data : 'unknown');
+      // Retorna um JSON válido mesmo em caso de erro
+      res.status(error.response ? error.response.status : 500).json({ error: 'Erro na requisição' });
+    }finally {
+     
+    }
+  }
