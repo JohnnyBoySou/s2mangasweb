3b09fab1d839a31ea44d1a15e696b0415250f8e7
JohnnyBoySou <joaodesousa101@gmail.com>
2024-05-30 17:40:47 -0300

    new data api

---
+++ src/data/weekend.js
@@ -0,0 +1,11 @@
+const weekend = [
+    {
+        id: 'd90ea6cb-7bc3-4d80-8af0-28557e6c4e17',
+        capa: 'https://mangadex.org/covers/d90ea6cb-7bc3-4d80-8af0-28557e6c4e17/d885a28e-a5d3-4dc3-85e9-ca600f227b04.jpg.512.jpg',
+        name: 'Dungeon Meshi',
+        score: '9.29',
+        type: 'manga'
+    }
+]
+
+export default weekend;
