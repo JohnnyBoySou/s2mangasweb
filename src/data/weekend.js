2f6eb103112346a6e876e275112275e8a4c22dbb
JohnnyBoySou <joaodesousa101@gmail.com>
2024-06-02 18:24:54 -0300

    new weekend mangas

--- src/data/weekend.js
+++ src/data/weekend.js
@@ -4,8 +4,37 @@ const weekend = [
         capa: 'https://mangadex.org/covers/d90ea6cb-7bc3-4d80-8af0-28557e6c4e17/d885a28e-a5d3-4dc3-85e9-ca600f227b04.jpg.512.jpg',
         name: 'Dungeon Meshi',
         score: '9.29',
-        type: 'manga'
-    }
+        type: 'Mangá'
+    },
+    {
+        id: 'c52b2ce3-7f95-469c-96b0-479524fb7a1a',
+        capa: 'https://mangadex.org/covers/c52b2ce3-7f95-469c-96b0-479524fb7a1a/a112e575-4f11-48b2-83d8-28057ceecc4c.jpg.512.jpg',
+        name: 'Jujutsu Kaisen',
+        score: '9.14',
+        type: 'Mangá'
+    },
+    {
+        id: '',
+        capa: '',
+        name: '',
+        score: '',
+        type: ''
+    },
+    {
+        id: '',
+        capa: '',
+        name: '',
+        score: '',
+        type: ''
+    },
+    {
+        id: '',
+        capa: '',
+        name: '',
+        score: '',
+        type: ''
+    },
+    
 ]
 
 export default weekend;
