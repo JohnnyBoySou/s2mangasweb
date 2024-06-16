f6a0c3041b4680568ff4a3b77db0e62e32fdb10c
JohnnyBoySou <joaodesousa101@gmail.com>
2024-06-16 13:58:03 -0300

    new mangas weekend

--- src/data/weekend.js
+++ src/data/weekend.js
@@ -14,25 +14,46 @@ const weekend = [
         type: 'Mangá'
     },
     {
-        id: '',
-        capa: '',
-        name: '',
-        score: '',
-        type: ''
+        id: '237d527f-adb5-420e-8e6e-b7dd006fbe47',
+        capa: 'https://mangadex.org/covers/237d527f-adb5-420e-8e6e-b7dd006fbe47/3af56897-f26d-4526-8b8d-d56b0da89e24.jpg.512.jpg',
+        name: 'Kaiju No. 8',
+        score: '8.85',
+        type: 'Mangá'
     },
     {
-        id: '',
-        capa: '',
-        name: '',
-        score: '',
-        type: ''
+        id: '9eb78304-0436-484d-9a79-a925b45e2731',
+        capa: 'https://mangadex.org/covers/9eb78304-0436-484d-9a79-a925b45e2731/927da800-8445-4240-bfd2-ccd754a3e044.jpg.512.jpg',
+        name: 'Wind Breaker',
+        score: '8.92',
+        type: 'Mangá'
     },
     {
-        id: '',
-        capa: '',
-        name: '',
-        score: '',
-        type: ''
+        id: 'e7eabe96-aa17-476f-b431-2497d5e9d060',
+        capa: 'https://mangadex.org/covers/e7eabe96-aa17-476f-b431-2497d5e9d060/f48d2c1f-c5a3-4e83-b6b4-8cb8b6912205.jpg.512.jpg',
+        name: 'Black Clover',
+        score: '8.64',
+        type: 'Mangá'
+    },
+    {
+        id: '6b1eb93e-473a-4ab3-9922-1a66d2a29a4a',
+        capa: 'https://mangadex.org/covers/6b1eb93e-473a-4ab3-9922-1a66d2a29a4a/bb223226-a5ca-457b-8a10-ad3ecaea66be.jpg.512.jpg',
+        name: 'Naruto',
+        score: '8.30',
+        type: 'Mangá'
+    },
+    {
+        id: '46e530ce-0766-4cbd-b005-5e6fb0ba5e71',
+        capa: 'https://mangadex.org/covers/46e530ce-0766-4cbd-b005-5e6fb0ba5e71/143198b5-58bd-49dd-b8d9-6060e8528e61.jpg.512.jpg',
+        name: 'Boruto: Naruto Next Generations',
+        score: '6.56',
+        type: 'Mangá'
+    },
+    {
+        id: '827a3cbf-6859-4f71-906f-285596becb59',
+        capa: 'https://mangadex.org/covers/827a3cbf-6859-4f71-906f-285596becb59/01b322f1-6b49-4971-86f9-6e6b8f9bdea7.jpg.512.jpg',
+        name: 'Nido to Jidori Okutte Yannai!',
+        score: '7.78',
+        type: 'Mangá'
     },
     
 ]
