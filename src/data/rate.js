2d6357cf0d9fbb6a056026c5a587fb37bc0a25c9
JohnnyBoySou <joaodesousa101@gmail.com>
2024-06-23 16:27:25 -0300

    rate add mangas

--- src/data/rate.js
+++ src/data/rate.js
@@ -4,8 +4,99 @@ const rate = [
         capa: 'https://mangadex.org/covers/d90ea6cb-7bc3-4d80-8af0-28557e6c4e17/d885a28e-a5d3-4dc3-85e9-ca600f227b04.jpg.512.jpg',
         name: 'Dungeon Meshi',
         score: '9.29',
-        type: 'manga'
-    }
+        type: 'Mangá'
+    },
+    {
+        id: 'd1a9fdeb-f713-407f-960c-8326b586e6fd',
+        capa: 'https://mangadex.org/covers/d1a9fdeb-f713-407f-960c-8326b586e6fd/05f8dcb4-8ea1-48db-a0b1-3a8fbf695e5a.jpg.512.jpg',
+        name: 'Vagabond',
+        score: '9.59',
+        type: 'Mangá',
+    },
+    {
+        id: '801513ba-a712-498c-8f57-cae55b38cc92',
+        capa: 'https://mangadex.org/covers/801513ba-a712-498c-8f57-cae55b38cc92/22ff2622-e93c-420f-b477-7a86eec02a1d.jpg.512.jpg',
+        name: 'Berserk',
+        score: '9.59',
+        type: 'Mangá',
+    },
+    {
+        id: '58b09ce2-ea05-405e-8e1c-a9361df9bdd9',
+        capa: 'https://mangadex.org/covers/58b09ce2-ea05-405e-8e1c-a9361df9bdd9/0606d23f-1507-4bd8-adc9-18c83329e9d0.jpg.512.jpg',
+        name: 'The Dawn to Come',
+        score: '9.50',
+        type: 'Mangá',
+    },
+    {
+        id: 'b73371d4-02dd-4db0-b448-d9afa3d698f1',
+        capa: 'https://mangadex.org/covers/b73371d4-02dd-4db0-b448-d9afa3d698f1/c4f630af-5509-4132-afcb-133374d95637.jpg.512.jpg',
+        name: 'Ao Ashi',
+        score: '9.51',
+        type: 'Mangá',
+    },
+    {
+        id: '9e03b2ca-5191-44a6-88b6-c0cd49d06b51',
+        capa: 'https://mangadex.org/covers/9e03b2ca-5191-44a6-88b6-c0cd49d06b51/2628b5d3-b121-4d47-ada7-c9dd3ba3e267.jpg.512.jpg',
+        name: 'I Sold My Life for 10,000 Yen per Year.',
+        score: '9.45',
+        type: 'Mangá',
+    },
+    {
+        id: '6da0b34b-db19-491a-b85c-6e31e0986f15',
+        capa: 'https://mangadex.org/covers/6da0b34b-db19-491a-b85c-6e31e0986f15/8e7603ba-924a-4fb3-9f46-3e1f4c5880a4.jpg.512.jpg',
+        name: 'Black Lagoon',
+        score: '9.38',
+        type: 'Mangá',
+    },
+    {
+        id: 'd9e30523-9d65-469e-92a2-302995770950',
+        capa: 'https://mangadex.org/covers/d9e30523-9d65-469e-92a2-302995770950/0b815f57-43ad-4782-a56f-bf9d0d034147.jpg.512.jpg',
+        name: 'Monster',
+        score: '9.35',
+        type: 'Mangá',
+    },
+    {
+        id: 'fed8c19c-9dec-4ef1-a187-af2c0c9cf17c',
+        capa: 'https://mangadex.org/covers/fed8c19c-9dec-4ef1-a187-af2c0c9cf17c/5f0fd818-ab19-4626-b7b3-c7440f63eb01.jpg.512.jpg',
+        name: 'Will You Marry Me Again If You Are Reborn?',
+        score: '9.35',
+        type: 'Mangá',
+    },
+    {
+        id: '6e3553b9-ddb5-4d37-b7a3-99998044774e',
+        capa: 'https://mangadex.org/covers/6e3553b9-ddb5-4d37-b7a3-99998044774e/e4504532-62f8-467c-9f90-d6390b5f3126.jpg.512.jpg',
+        name: 'ReLIFE',
+        score: '9.35',
+        type: 'Mangá',
+    },
+    {
+        id: '4bde51e5-e420-45a4-98e9-7405bf2d59ff',
+        capa: 'https://mangadex.org/covers/4bde51e5-e420-45a4-98e9-7405bf2d59ff/2f8d133a-cf64-442a-9c79-cde975f6ae0c.jpg.512.jpg',
+        name: 'Koe no Katachi',
+        score: '9.34',
+        type: 'Mangá',
+    },
+    {
+        id: '319df2e2-e6a6-4e3a-a31c-68539c140a84',
+        capa: 'https://mangadex.org/covers/319df2e2-e6a6-4e3a-a31c-68539c140a84/9cd70b08-eaf4-45cf-9e59-91afd3fd1781.jpg.512.jpg',
+        name: 'Slam Dunk!',
+        score: '9.34',
+        type: 'Mangá',
+    },
+    {
+        id: '8847f905-550d-4fe6-bcda-ac2b896789c7',
+        capa: 'https://mangadex.org/covers/8847f905-550d-4fe6-bcda-ac2b896789c7/e96cfe6d-7876-4558-b4bc-0930deb5060a.jpg.512.jpg',
+        name: 'Golden Kamuy',
+        score: '9.34',
+        type: 'Mangá',
+    },
+    {
+        id: 'bc634b22-0c05-4a55-9b7e-3f55438a1ad3',
+        capa: 'https://mangadex.org/covers/bc634b22-0c05-4a55-9b7e-3f55438a1ad3/e9298c0d-5884-42c8-bdbc-7f1f60c6f7b1.jpg.512.jpg',
+        name: 'Gekkan Shoujo Nozaki-kun',
+        score: '9.34',
+        type: 'Mangá',
+    },
 ]
 
 export default rate;
