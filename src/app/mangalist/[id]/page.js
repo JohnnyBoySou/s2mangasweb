8a82015601cc93aeaee1e676aa283a7201d69dbe
JohnnyBoySou <joaodesousa101@gmail.com>
2024-06-30 13:25:27 -0300

    mangalist adjust

--- src/app/mangalist/[id]/page.js
+++ src/app/mangalist/[id]/page.js
@@ -1,11 +1,11 @@
 'use client';
 import React, { useEffect,  useState} from 'react';
-import { Column, Row, Title, Label, } from '../../../themes/global';
+import { Column, Row, Title, Label, } from '@themes/global';
 import './details.css' 
-import stories from '../../../requests/mangalist';
+import stories from '@data/mangalists';
 import Image from 'next/image';
-import ListMangalist from '../../../components/Cards/list_mangalist';
-import NavBar from '../../../components/NavBar';
+import ListMangalist from '@components/Cards/list_mangalist';
+import NavBar from '@components/NavBar';
 
 export default function MangaListDetails({ params }) {
     const id = Number(params.id);
@@ -21,7 +21,7 @@ export default function MangaListDetails({ params }) {
                     <Image src={item?.capa} alt="capa da mangalist" width={200} height={200} style={{borderRadius: 8, objectFit: 'cover'}}/>
                 </Column>
                 <Column style={{justifyContent: 'center', marginLeft: 44,}}>
-                        <Label style={{ color: "#ffffff90"}}>Mangálist • {item?.date} • {item?.mangas_ids.length} mangás</Label>
+                        <Label style={{ color: "#ffffff90"}}>Mangálist • {item?.date} • {item?.mangas_ids?.length} mangás</Label>
                         <Title   style={{  fontSize: 54, background: "#30303000", border: 'none', fontFamily: 'Bold', color: "#fff",}}> {item?.name}</Title>
                         <Label style={{fontSize: 24,}}>{item?.desc} </Label>
                 </Column>
@@ -32,8 +32,8 @@ export default function MangaListDetails({ params }) {
     
         <Column style={{paddingTop: 40, borderRadius: 24, backgroundColor: '#202020', marginTop: -30,}}>
             <Title style={{marginLeft: 44, fontSize: 42, marginBottom: 20,}}>Mangás</Title>
-            {item?.mangas_ids.length === 0 && <NotFound />}
-            <ListMangalist data={item.mangas_ids} />
+            {item?.mangas_ids?.length === 0 && <NotFound />}
+            <ListMangalist data={item?.mangas_ids} />
         </Column>
          </Column>
         
