32a7bbb4885a7badbac527835c2b936e9633dfd3
JohnnyBoySou <joaodesousa101@gmail.com>
2024-08-17 12:39:15 -0300

    adjsut color

--- src/app/politica-de-privacidade/page.js
+++ src/app/politica-de-privacidade/page.js
@@ -1,28 +1,29 @@
 import React from 'react';
+import { Title, Label, Column } from '@theme/global';
 
 const PrivacyPolicy = () => {
   return (
-    <div style={{ padding: '20px', maxWidth: '800px', margin: '0 auto' }}>
-      <h1>Política de Privacidade - S2 Mangás</h1>
-      <p>No <strong>S2 Mangás</strong>, levamos sua privacidade a sério. Queremos que você entenda como lidamos com seus dados pessoais.</p>
-      
-      <h2>Coleta de Dados</h2>
-      <p>Coletamos apenas informações necessárias para melhorar sua experiência no aplicativo, como:</p>
+    <Column style={{ padding: '20px', maxWidth: '800px', margin: '0 auto' }}>
+      <Title>Política de Privacidade - S2 Mangás</Title>
+      <Label>No <strong>S2 Mangás</strong>, levamos sua privacidade a sério. Queremos que você entenda como lidamos com seus dados pessoais.</Label>
+
+      <Title>Coleta de Dados</Title>
+      <Label>Coletamos apenas informações necessárias para melhorar sua experiência no aplicativo, como:</Label>
       <ul>
         <li>Preferências de leitura</li>
         <li>Configurações de coleções</li>
         <li>Personalização de conta</li>
       </ul>
-      <p>Esses dados são <strong>armazenados localmente</strong> em seu dispositivo para garantir que suas preferências sejam mantidas.</p>
-      
-      <h2>Não enviamos dados para a web</h2>
-      <p>Nenhuma informação coletada por nós é enviada ou compartilhada com servidores externos, serviços de terceiros ou a internet. Tudo o que você faz no app fica apenas no seu dispositivo.</p>
+      <Label>Esses dados são <strong>armazenados localmente</strong> em seu dispositivo para garantir que suas preferências sejam mantidas.</Label>
 
-      <h2>Alterações nesta Política</h2>
-      <p>Podemos atualizar nossa Política de Privacidade ocasionalmente. Qualquer alteração será informada diretamente no aplicativo.</p>
+      <Title>Não enviamos dados para a web</Title>
+      <Label>Nenhuma informação coletada por nós é enviada ou compartilhada com servidores externos, serviços de terceiros ou a internet. Tudo o que você faz no app fica apenas no seu dispositivo.</Label>
 
-      <p><strong>Última atualização:</strong> [data]</p>
-    </div>
+      <Title>Alterações nesta Política</Title>
+      <Label>Podemos atualizar nossa Política de Privacidade ocasionalmente. Qualquer alteração será informada diretamente no aplicativo.</Label>
+
+      <Label><strong>Última atualização:</strong> [data]</Label>
+    </Column>
   );
 };
 
