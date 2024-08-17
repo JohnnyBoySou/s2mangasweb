e4431025be0b2a84288064a4a1e455440e68eb2b
JohnnyBoySou <joaodesousa101@gmail.com>
2024-08-17 13:08:08 -0300

    adjsuts politica

--- src/app/politica-de-privacidade/page.js
+++ src/app/politica-de-privacidade/page.js
@@ -4,15 +4,15 @@ import { Title, Label, Column } from '@theme/global';
 const PrivacyPolicy = () => {
   return (
     <Column style={{ padding: '20px', maxWidth: '800px', margin: '0 auto' }}>
-      <Title>Política de Privacidade - S2 Mangás</Title>
+      <Title size={32}>Política de Privacidade - S2 Mangás</Title>
       <Label>No <strong>S2 Mangás</strong>, levamos sua privacidade a sério. Queremos que você entenda como lidamos com seus dados pessoais.</Label>
 
       <Title>Coleta de Dados</Title>
       <Label>Coletamos apenas informações necessárias para melhorar sua experiência no aplicativo, como:</Label>
-      <ul>
-        <li>Preferências de leitura</li>
-        <li>Configurações de coleções</li>
-        <li>Personalização de conta</li>
+      <ul style={{ color: '#ffffff' }}>
+        <li style={{ color: '#ffffff' }}>Preferências de leitura</li>
+        <li style={{ color: '#ffffff' }}>Configurações de coleções</li>
+        <li style={{ color: '#ffffff' }}>Personalização de conta</li>
       </ul>
       <Label>Esses dados são <strong>armazenados localmente</strong> em seu dispositivo para garantir que suas preferências sejam mantidas.</Label>
 
@@ -22,7 +22,7 @@ const PrivacyPolicy = () => {
       <Title>Alterações nesta Política</Title>
       <Label>Podemos atualizar nossa Política de Privacidade ocasionalmente. Qualquer alteração será informada diretamente no aplicativo.</Label>
 
-      <Label><strong>Última atualização:</strong> [data]</Label>
+      <Label><strong>Última atualização:</strong> 17/08/2024</Label>
     </Column>
   );
 };
