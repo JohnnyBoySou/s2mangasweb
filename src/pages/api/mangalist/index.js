2983f08e5a99f1effe7e454ec24b7ec0412d25ab
JohnnyBoySou <joaodesousa101@gmail.com>
2024-08-26 11:48:53 -0300

    adjust api mangalist

--- src/pages/api/mangalist/index.js
+++ src/pages/api/mangalist/index.js
@@ -3,11 +3,14 @@ import stories from "@data/mangalists";
 export default async function handler(req, res) {
   try {
     const { page, type } = req.query;
-    const storiesByType = stories.filter((story) => story.type === type);
-    const pageSize = 20;
+   // const storiesByType = stories.filter((story) => story.type === type);
+    
+   const pageSize = 20;
     const startIndex = ((page || 1) - 1) * pageSize;
     const endIndex = (page || 1) * pageSize;
-    const paginatedStories = storiesByType.slice(startIndex, endIndex);
+    
+    const paginatedStories = stories.slice(startIndex, endIndex);
+    
     res.status(200).json(paginatedStories);
   } catch (error) {
     console.error(error.message);
