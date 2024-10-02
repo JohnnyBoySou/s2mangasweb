23052848d797765deec4c6d9816b08ee0e6670ac
JohnnyBoySou <joaodesousa101@gmail.com>
2024-10-02 05:17:14 -0300

    new wallpapers

--- src/data/wallpapers/index.js
+++ src/data/wallpapers/index.js
@@ -1,73 +1,154 @@
 
 const wallpapers = [
     {
+        name: 'Naruto',
         id: 1,
-        img: 'https://i.pinimg.com/736x/f1/1f/89/f11f895c6e37804b56bdc43ca168886a.jpg',
+        capa: '',
+        data: [
+            {
+                id: 1,
+                img: 'https://i.pinimg.com/736x/f1/1f/89/f11f895c6e37804b56bdc43ca168886a.jpg',
+            },
+            {
+                id: 2,
+                img: 'https://i.pinimg.com/736x/fa/28/cf/fa28cfa07d9291779866aadb2e273af0.jpg',
+            },
+            {
+                id: 3,
+                img: 'https://i.pinimg.com/736x/28/01/1f/28011f4ae0c38a021215607f38fa95b2.jpg',
+            },
+            {
+                id: 4,
+                img: 'https://i.pinimg.com/736x/4e/02/29/4e02291237ad1940918dc11aeeb435ac.jpg',
+            },
+            {
+                id: 5,
+                img: 'https://i.pinimg.com/736x/90/fc/5d/90fc5d8ac81d2a2158c477b88dc386dd.jpg',
+            },
+            {
+                id: 6,
+                img: 'https://i.pinimg.com/736x/c7/9c/cd/c79ccd7493fbf7887fb21e4cc368dc42.jpg',
+            },
+            {
+                id: 7,
+                img: 'https://i.pinimg.com/736x/38/d1/92/38d192513d275012079c927703dfda13.jpg',
+            },
+            {
+                id: 8,
+                img: 'https://i.pinimg.com/736x/ef/3b/6a/ef3b6aa55360c37bf1b68d1418641f62.jpg',
+            },
+            {
+                id: 9,
+                img: 'https://i.pinimg.com/736x/1d/21/5b/1d215b29afdf660c743b64230488ebdf.jpg',
+            },
+            {
+                id: 10,
+                img: 'https://i.pinimg.com/736x/cd/17/06/cd1706dd485a17eec77eaedf1229b391.jpg',
+            },
+            {
+                id: 11,
+                img: 'https://i.pinimg.com/736x/0d/2e/5d/0d2e5d7ade44dede9c0f5aad43467db2.jpg',
+            },
+            {
+                id: 12,
+                img: 'https://i.pinimg.com/736x/39/b0/6d/39b06d98fe7a0a4c822fd0d631b10ad3.jpg',
+            },
+            {
+                id: 13,
+                img: 'https://i.pinimg.com/736x/2f/51/02/2f51029a85427cc6b33636df4d7ef225.jpg',
+            },
+            {
+                id: 14,
+                img: 'https://i.pinimg.com/736x/6d/f2/9c/6df29c464490b1c1b583f487c52f9cbc.jpg',
+            },
+            {
+                id: 15,
+                img: 'https://i.pinimg.com/736x/f9/de/ab/f9deab4dc114c1be1c7bb62d9440ff0b.jpg',
+            },
+            {
+                id: 16,
+                img: 'https://i.pinimg.com/736x/7c/f9/d2/7cf9d27193ae40f86ffb83394587b326.jpg',
+            },
+            {
+                id: 17,
+                img: 'https://i.pinimg.com/736x/8f/65/43/8f6543b15e747ebfc136006f70dc5a2c.jpg'
+            },
+            {
+                id: 18,
+                img: 'https://i.pinimg.com/564x/a9/cf/8f/a9cf8ffb67d6f717ce5cae1ec44b2575.jpg',
+            },
+        ],
     },
+
     {
+        name: 'Dr Stone',
+        capa: '',
         id: 2,
-        img: 'https://i.pinimg.com/736x/fa/28/cf/fa28cfa07d9291779866aadb2e273af0.jpg',
+        data: [
+            {
+                id: 1,
+                img: '',
+            },
+            {
+                id: 2,
+                img: '',
+            },
+            {
+                id: 3,
+                img: '',
+            },
+            {
+                id: 4,
+                img: '',
+            },
+            {
+                id: 5,
+                img: '',
+            },
+            {
+                id: 6,
+                img: '',
+            },
+            {
+                id: 7,
+                img: '',
+            },
+            {
+                id: 8,
+                img: '',
+            },
+            {
+                id: 9,
+                img: '',
+            },
+            {
+                id: 10,
+                img: '',
+            },
+            {
+                id: 11,
+                img: '',
+            },
+            {
+                id: 12,
+                img: '',
+            },
+            {
+                id: 13,
+                img: '',
+            },
+            {
+                id: 14,
+                img: '',
+            },
+            {
+                id: 15,
+                img: '',
+            },
+        ]
+
     },
-    {
-        id: 3,
-        img: 'https://i.pinimg.com/736x/28/01/1f/28011f4ae0c38a021215607f38fa95b2.jpg',
-    },
-    {
-        id: 4,
-        img: 'https://i.pinimg.com/736x/4e/02/29/4e02291237ad1940918dc11aeeb435ac.jpg',
-    },
-    {
-        id: 5,
-        img: 'https://i.pinimg.com/736x/90/fc/5d/90fc5d8ac81d2a2158c477b88dc386dd.jpg',
-    },
-    {
-        id: 6,
-        img: 'https://i.pinimg.com/736x/c7/9c/cd/c79ccd7493fbf7887fb21e4cc368dc42.jpg',
-    },
-    {
-        id: 7,
-        img: 'https://i.pinimg.com/736x/38/d1/92/38d192513d275012079c927703dfda13.jpg',
-    },
-    {
-        id: 8,
-        img: 'https://i.pinimg.com/736x/ef/3b/6a/ef3b6aa55360c37bf1b68d1418641f62.jpg',
-    },
-    {
-        id: 9,
-        img: 'https://i.pinimg.com/736x/1d/21/5b/1d215b29afdf660c743b64230488ebdf.jpg',
-    },
-    {
-        id: 10,
-        img: 'https://i.pinimg.com/736x/cd/17/06/cd1706dd485a17eec77eaedf1229b391.jpg',
-    },
-    {
-        id: 11,
-        img: 'https://i.pinimg.com/736x/0d/2e/5d/0d2e5d7ade44dede9c0f5aad43467db2.jpg',
-    },
-    {
-        id: 12,
-        img: 'https://i.pinimg.com/736x/39/b0/6d/39b06d98fe7a0a4c822fd0d631b10ad3.jpg',
-    },
-    {
-        id: 13,
-        img: 'https://i.pinimg.com/736x/2f/51/02/2f51029a85427cc6b33636df4d7ef225.jpg',
-    },
-    {
-        id: 14,
-        img: 'https://i.pinimg.com/736x/6d/f2/9c/6df29c464490b1c1b583f487c52f9cbc.jpg',
-    },
-    {
-        id: 15,
-        img: 'https://i.pinimg.com/736x/f9/de/ab/f9deab4dc114c1be1c7bb62d9440ff0b.jpg',
-    },
-    {
-        id: 16,
-        img: 'https://i.pinimg.com/736x/7c/f9/d2/7cf9d27193ae40f86ffb83394587b326.jpg',
-    },
-    {
-        id: 17,
-        img: 'https://i.pinimg.com/736x/8f/65/43/8f6543b15e747ebfc136006f70dc5a2c.jpg'
-    }
+
 ]
 
 export default wallpapers;
