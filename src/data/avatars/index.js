33e8c3eba9ccfc5fd6be71cc066132baef838be3
João Sousa <49738909+JohnnyBoySou@users.noreply.github.com>
2024-11-09 13:01:16 -0300

    ADD NEW AVATARS

--- src/data/avatars/index.js
+++ src/data/avatars/index.js
@@ -3,88 +3,44 @@ const avatars = [
     {
         name: 'Naruto',
         id: 1,
-        capa: 'https://i.pinimg.com/736x/16/ec/d4/16ecd447df34950052cc634f8e0e0146.jpg',
+        capa: 'https://i.pinimg.com/736x/c6/c9/e9/c6c9e9827b22444591f948d3a288e3a3.jpg',
         data: [
             {
                 id: 1,
-                img: 'https://i.pinimg.com/736x/33/37/9d/33379d882ffdd994a2750caaf6aa91ed.jpg',
+                img: 'https://i.pinimg.com/236x/13/0b/08/130b08692313b876d2a42b8de9052dbf.jpg',
             },
             {
                 id: 2,
-                img: 'https://i.pinimg.com/736x/32/2b/b6/322bb641b35064ef5cf21a6e9dda91a5.jpg',
+                img: 'https://i.pinimg.com/236x/69/08/3a/69083aa75f6f03a9f4126c90ad7a8819.jpg',
             },
-            {
-                id: 3,
-                img: 'https://i.pinimg.com/736x/9c/e7/78/9ce778c70354a5b6ae4310805357d58c.jpg',
-            },
-            {
-                id: 4,
-                img: 'https://i.pinimg.com/736x/4e/02/29/4e02291237ad1940918dc11aeeb435ac.jpg',
-            },
-            {
-                id: 5,
-                img: 'https://i.pinimg.com/736x/4b/dc/fd/4bdcfd6aa5ad1264701e952148250abe.jpg',
-            },
-            {
-                id: 6,
-                img: 'https://i.pinimg.com/736x/c7/9c/cd/c79ccd7493fbf7887fb21e4cc368dc42.jpg',
-            },
-            {
-                id: 7,
-                img: 'https://i.pinimg.com/736x/38/d1/92/38d192513d275012079c927703dfda13.jpg',
-            },
-            {
-                id: 8,
-                img: 'https://i.pinimg.com/736x/ef/3b/6a/ef3b6aa55360c37bf1b68d1418641f62.jpg',
-            },
-            {
-                id: 9,
-                img: 'https://i.pinimg.com/736x/1d/21/5b/1d215b29afdf660c743b64230488ebdf.jpg',
-            },
-            {
-                id: 10,
-                img: 'https://i.pinimg.com/736x/cd/17/06/cd1706dd485a17eec77eaedf1229b391.jpg',
-            },
-            {
-                id: 11,
-                img: 'https://i.pinimg.com/736x/0d/2e/5d/0d2e5d7ade44dede9c0f5aad43467db2.jpg',
-            },
-            {
-                id: 12,
-                img: 'https://i.pinimg.com/736x/39/b0/6d/39b06d98fe7a0a4c822fd0d631b10ad3.jpg',
-            },
-            {
-                id: 13,
-                img: 'https://i.pinimg.com/736x/2f/51/02/2f51029a85427cc6b33636df4d7ef225.jpg',
-            },
-            {
-                id: 14,
-                img: 'https://i.pinimg.com/736x/6d/f2/9c/6df29c464490b1c1b583f487c52f9cbc.jpg',
-            },
-            {
-                id: 15,
-                img: 'https://i.pinimg.com/736x/f9/de/ab/f9deab4dc114c1be1c7bb62d9440ff0b.jpg',
-            },
-            {
-                id: 16,
-                img: 'https://i.pinimg.com/736x/7c/f9/d2/7cf9d27193ae40f86ffb83394587b326.jpg',
-            },
-            {
-                id: 17,
-                img: 'https://i.pinimg.com/736x/8f/65/43/8f6543b15e747ebfc136006f70dc5a2c.jpg'
-            },
-            {
-                id: 18,
-                img: 'https://i.pinimg.com/736x/2d/e1/bf/2de1bf9d4b1d55d64ce52543fcf59e48.jpg',
-            },
-            {
-                id: 19,
-                img: 'https://i.pinimg.com/736x/16/ec/d4/16ecd447df34950052cc634f8e0e0146.jpg',
-            },
-            {
-                id: 20,
-                img: 'https://i.pinimg.com/736x/e9/d1/c2/e9d1c2e7a4084b7241e0a6a5098ccdc0.jpg',
-            },
+            { id: 3, img: 'https://i.pinimg.com/236x/b4/64/3c/b4643c8a9b000777fe877899d3b90045.jpg'},
+            { id: 4, img: 'https://i.pinimg.com/236x/0f/a4/f9/0fa4f920ff1ee8376d90ff207f48c8e2.jpg'},
+            { id: 5, img: 'https://i.pinimg.com/236x/8c/36/7f/8c367f7af483063ffc5506715abfd4dc.jpg'},
+            { id: 6, img: 'https://i.pinimg.com/236x/d5/bc/be/d5bcbed338a1d60ea954ae9dfe2ce711.jpg'},
+            { id: 7, img: 'https://i.pinimg.com/236x/03/93/0b/03930be3fbe1e0f65eb11a3229659b4b.jpg'},
+            { id: 8, img: 'https://i.pinimg.com/236x/a7/b6/b2/a7b6b2c0cd5f6e8fdf50e71e484bf2df.jpg'},
+            { id: 9, img: 'https://i.pinimg.com/236x/68/b5/93/68b593d2cea9b66084a4e572cf33e089.jpg'},
+            { id: 10, img: 'https://i.pinimg.com/236x/38/09/c9/3809c967a49926f3217a11d115e50d86.jpg'},
+            { id: 11, img: 'https://i.pinimg.com/236x/4f/06/18/4f06183ccdea4275966860fe91385836.jpg'},
+            { id: 12, img: 'https://i.pinimg.com/564x/70/fa/31/70fa31c21e59f3d0b5b28fb38b9582ea.jpg'},
+            { id: 13, img: 'https://i.pinimg.com/236x/f1/5e/13/f15e138b7709a021515b157a76add674.jpg'},
+            { id: 14, img: 'https://i.pinimg.com/236x/90/08/4b/90084b40889cb8677e028cf9d7afbc84.jpg'},
+            { id: 15, img: 'https://i.pinimg.com/236x/6c/8b/8b/6c8b8b8f90bddb044978db8e76763464.jpg'},
+            { id: 16, img: 'https://i.pinimg.com/474x/c6/c9/e9/c6c9e9827b22444591f948d3a288e3a3.jpg'},
+            { id: 17, img: 'https://i.pinimg.com/236x/92/b9/6b/92b96b5b600b77f11d0a67f3064be7f9.jpg'},
+            { id: 18, img: 'https://i.pinimg.com/736x/f3/42/08/f3420833741b6793e131fa5eef8f9fd3.jpg'},
+            { id: 19, img: 'https://i.pinimg.com/236x/cb/3f/d2/cb3fd272c09a8ee81e13c9fa71ffc5f1.jpg'},
+            { id: 20, img: 'https://i.pinimg.com/236x/f8/cb/fc/f8cbfc5b4ed21cd1a718786966568970.jpg'},
+            { id: 21, img: 'https://i.pinimg.com/474x/45/26/5c/45265c3481b5eea70601615706da8078.jpg'},
+            { id: 22, img: 'https://i.pinimg.com/236x/63/f6/70/63f670518d0edfb38cb1522da45e00cc.jpg'},
+            { id: 23, img: 'https://i.pinimg.com/474x/10/43/f4/1043f44e55140b24bcbfbc5747c0cfa3.jpg'},
+            { id: 24, img: 'https://i.pinimg.com/236x/5b/9f/3e/5b9f3edbf8aa6e64af9f5259ea11c705.jpg'},
+            { id: 25, img: 'https://i.pinimg.com/474x/d6/ef/81/d6ef8144761a2eed4e9d6a26b231dcca.jpg'},
+            { id: 26, img: 'https://i.pinimg.com/236x/c9/cc/6d/c9cc6dea7e21a65f380046a9fa9efa15.jpg'},
+            { id: 27, img: 'https://i.pinimg.com/236x/02/ab/6f/02ab6fa77539df1a1b3a3ad9cdcddb2b.jpg'},
+            { id: 28, img: 'https://i.pinimg.com/236x/16/b7/5d/16b75dd0bc902dca4deaae1851593fc5.jpg'},
+            { id: 29, img: 'https://i.pinimg.com/474x/e9/8b/4c/e98b4c4f4fa234b40104660e40f05ae3.jpg'},
+            { id: 30, img: 'https://i.pinimg.com/474x/37/08/cf/3708cfb9aafac7e0c9ad5dc21a35c656.jpg'},
         ],
     },
     {
