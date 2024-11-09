4044d255bd892ddb21c828bf01bac3b880868a7b
João Sousa <49738909+JohnnyBoySou@users.noreply.github.com>
2024-11-09 11:59:49 -0300

    add banners

---
+++ src/pages/api/banners/index.js
@@ -0,0 +1,19 @@
+import banners from "@data/banners/index";
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  try {
+    res.status(200).json({ banners: banners });
+  } catch (error) {
+    console.error('Axios error:', error.message);
+    console.error('Status:', error.response ? error.response.status : 'unknown');
+    console.error('Data:', error.response ? error.response.data : 'unknown');
+    // Retorna um JSON válido mesmo em caso de erro
+    res.status(error.response ? error.response.status : 500).json({ error: 'Erro na requisição' });
+  } finally {
+
+  }
+}
